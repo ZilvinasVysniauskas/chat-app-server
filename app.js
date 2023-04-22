502bce9f2d5803d2ca7367b4c23255e0917af574
ZilvinasVysniauskas <zilvinas.vysniauskas99@gmail.com>
2023-04-22 12:40:29 +0300

    set port to 8081

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const exampleRouter = require('./src/routes/exampleRoutes');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 
 app.use(express.json());
 app.use('/test', exampleRouter);
