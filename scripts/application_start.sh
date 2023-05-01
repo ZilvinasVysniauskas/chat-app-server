2f3125277ccf7af6ed80c31109b46e2de6fffaa9
ZilvinasVysniauskas <zilvinas.vysniauskas99@gmail.com>
2023-05-01 20:19:46 +0300

    good try

---
+++ scripts/application_start.sh
@@ -0,0 +1,6 @@
+!/bin/bash
+
+# Move to the directory
+cd /home/ec2-user/content-chat-server
+
+pm2 reload 0
---
+++ scripts/install_dependencies.sh
@@ -0,0 +1,10 @@
+!/bin/bash
+
+# Move to the directory
+cd /home/ec2-user/content-chat-server
+
+# Install dependencies
+npm i
+
+# Build the Code
+npm run build
