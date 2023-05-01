8db72393ecbfe0be309ef480221a114e08f3f76c
ZilvinasVysniauskas <zilvinas.vysniauskas99@gmail.com>
2023-05-01 13:25:49 +0300

    some build

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const exampleRouter = require('./src/routes/exampleRoutes');
 
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use('', exampleRouter);
