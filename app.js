430a8ee2574c218c6f72b0fd6b0d023d66fd91e9
ZilvinasVysniauskas <zilvinas.vysniauskas99@gmail.com>
2023-05-03 23:06:40 +0300

    changed port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const exampleRouter = require('./src/routes/exampleRoutes');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3000;
 
 app.use(express.json());
 app.use('', exampleRouter);
