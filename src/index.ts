05fee2c4c649b47dc20cc7ce4901abf651c1b9b3
ZilvinasVysniauskas <zilvinas.vysniauskas99@gmail.com>
2023-05-06 18:11:29 +0300

    added some test

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express, { Application } from 'express';
 import userRoutes from './routes/users-routes';
+import testEnv from './test/test-to-delete';
 
 const app: Application = express();
 app.use(express.json());
 app.use('/users', userRoutes);
+
+app.use('/test', testEnv)
 
 export default app;
